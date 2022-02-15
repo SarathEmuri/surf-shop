ca11a57987009e359c0a684b379a7a172213d974
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-15 15:29:53 -0500

    Worked on Posts Update

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,7 +14,7 @@ module.exports = {
     // Posts Create
     async postCreate(req, res, next) {
         //use req.body to create new Post
-        let post = await Post.create(req.body);
+        let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
     //Posts Show
@@ -26,5 +26,10 @@ module.exports = {
     async postEdit(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/edit', { post });
+    },
+    // Posts Update
+    async postUpdate(req, res, next) {
+        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, { new: true});
+        res.redirect(`/posts/${post.id}`);
     }
 }
