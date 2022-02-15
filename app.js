62914f64405e26e4937a132780b357f75910aaa2
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-15 07:06:01 -0500

    Installed passport-local, passport-local-mongoose & express-session using npm. Also configured these on app.js file.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const User = require('./models/user');
 
+// require routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
@@ -21,6 +24,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure Passport and Sessions
+app.use(session({
+  secret: 'first git project',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', postsRouter);
