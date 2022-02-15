9fd39518a30a29d298f94e183aac93d56e78187f
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-15 12:17:14 -0500

    Refactored login and logout routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+const passport = require('passport');
 const User = require('../models/user');
 
 module.exports = {
@@ -7,8 +8,20 @@ module.exports = {
           email: req.body.email,
           image: req.body.image
         });
-        
+
         await User.register(newUser, req.body.password);
         res.redirect('/');
+    },
+
+    postLogin(req, res, next) {
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+      })(req, res, next);
+    },
+
+    getLogout(req, res, next) {
+      req.logout();
+      res.redirect('/');
     }
 }
