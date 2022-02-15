f812cc9896895c53899dfc4642323324dfef8787
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-15 08:29:57 -0500

    Connected mongoDB to surf-shop and made some code changes in app.js for mongoose

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
+const mongoose = require('mongoose');
 
 // require routes
 const indexRouter = require('./routes/index');
@@ -14,6 +15,15 @@ const reviewsRouter = require('./routes/reviews');
 // const usersRouter = require('./routes/users');
 
 const app = express();
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/surf-shop', { useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('We are connected.');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
